bf8238fdc2f1a299e7deb31d58f1bb770708049a
William Marques <williamarques91@gmail.com>
2021-02-23 04:06:00 +0100

    update ng CLI to v11

--- src/app/home/edit-meals-dialog/edit-meals-dialog.component.spec.ts
+++ src/app/home/edit-meals-dialog/edit-meals-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EditMealsDialog } from './edit-meals-dialog.component';
 
@@ -6,7 +6,7 @@ describe('EditMealsDialogComponent', () => {
   let component: EditMealsDialog;
   let fixture: ComponentFixture<EditMealsDialog>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [EditMealsDialog]
     }).compileComponents();
