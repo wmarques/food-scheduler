5f6f3ab9e8d264f6087e47cf27fb538ca3603f97
William Marques <wmarques.pro@gmail.com>
2022-01-23 19:03:45 +0100

    bump to ng13

--- src/app/summary/summary.module.ts
+++ src/app/summary/summary.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SummaryComponent } from './summary.component';
-import { SummaryRoutingModule } from './summary-routing.module';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SummaryComponent} from './summary.component';
+import {SummaryRoutingModule} from './summary-routing.module';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [SummaryComponent],
@@ -18,8 +16,6 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     SummaryRoutingModule,
     MatFormFieldModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
     MatInputModule,
     MatButtonModule,
     MatDividerModule,
